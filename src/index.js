02a9799b3497147e74e97a65bf16c1a9e5a9a7b5
최지설 <121646920+choijisul@users.noreply.github.com>
2024-07-31 11:23:54 +0900

    컴포넌트와 props 실습 완료

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // import Library from './chapter_03/Library';  //chap03에 사용
-import Clock from './chapter_04/Clock';
+// import Clock from './chapter_04/Clock';  //chap04에 사용
+import CommentList from './chapter_05/CommentList';  //chap05에 사용
 
 // chap03에 사용
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,14 +17,22 @@ import Clock from './chapter_04/Clock';
 // );
 
 // chap04에 사용
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// setInterval(()=> {
+//     root.render(
+//         <React.StrictMode>
+//             <Clock/>
+//         </React.StrictMode>
+//     );
+// }, 1000);
+
+// chap05에 사용
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(()=> {
-    root.render(
-        <React.StrictMode>
-            <Clock/>
-        </React.StrictMode>
-    );
-}, 1000);
+root.render(
+    <React.StrictMode>
+        <CommentList/>
+    </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
