3df1c77d7579a60da845a3fb9a0233933b5a0f85
최지설 <121646920+choijisul@users.noreply.github.com>
2024-08-01 13:02:54 +0900

    state와 생명주기 실습 완료

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import reportWebVitals from './reportWebVitals';
 
 // import Library from './chapter_03/Library';  //chap03에 사용
 // import Clock from './chapter_04/Clock';  //chap04에 사용
-import CommentList from './chapter_05/CommentList';  //chap05에 사용
+// import CommentList from './chapter_05/CommentList';  //chap05에 사용
+import NotificationList from './chapter_06/NotificationList';  //chap06에 사용
 
-// chap03에 사용
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <React.StrictMode>
-//         <Library />
-//     </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        {/* <Library />  chap03에 사용 */}
+        {/* <CommentList/>  chap05에 사용 */}
+        <NotificationList/>
+    </React.StrictMode>
+);
 
 // chap04에 사용
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -26,15 +28,4 @@ import CommentList from './chapter_05/CommentList';  //chap05에 사용
 //     );
 // }, 1000);
 
-// chap05에 사용
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <CommentList/>
-    </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
